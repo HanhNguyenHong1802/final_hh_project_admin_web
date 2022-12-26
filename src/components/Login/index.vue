291404f56c50acb7cf8e2cd9bad7710399ae2bb0
hani.nguyen <hanhnguyee2000@j2.team>
2022-12-27 00:52:42 +0700

    Initial commit

---
+++ src/components/Login/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-red-100">hello</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "LoginPage",
+};
+</script>
---
+++ src/store/modules/user-module.ts
@@ -0,0 +1,15 @@
+// initial state
+
+// const state = () => ({
+//   user: {},
+// });
+
+//getters
+// const getters = {};
+
+// actions
+// const actions = {
+//   async getUser ({commit}) {
+//     const user = await
+//   }
+// }
